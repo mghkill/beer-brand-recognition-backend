30df97e7853750509cfe9e35c7cf5d66bff28eec
Filipe Melo <filipexpert63@gmail.com>
2024-09-30 10:57:37 -0300

    Microservice-Flask finalizado.

--- Microservice_Flask/app/controllers/controllers_uploads.py
+++ Microservice_Flask/app/controllers/controllers_uploads.py
@@ -3,14 +3,19 @@ from flask import jsonify, request
 from app.services.services_uploads import handle_upload, handle_error
 
 def create_uploads():
-    data = request.get_json()
+     # Recebe o arquivo enviado
+    file = request.files.get('file')  # O nome 'file' deve corresponder ao que foi enviado no frontend
     
-    try:
-        response_data, status = handle_upload(data)
-        return jsonify(response_data), status
-    except (TypeError, KeyError) as e:
-        return handle_error(e)
+    if file:
+        # Para verificar se a imagem foi recebida com sucesso
+        print(f"File received: {file.filename}")
 
+        # Aqui você pode passar o arquivo para a função handle_upload
+        response_data, status = handle_upload(file)
+        return jsonify(response_data), status
+    else:
+        return jsonify({"message": "No file received."}), HTTPStatus.BAD_REQUEST
+    
 def read_all_uploads():
     pass
 
